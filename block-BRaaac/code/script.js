d4726f0ea297fca333c0522b6e1d5dc4668fafa4
umakant-2113 <lodhiumakant800@gmail.com>
2022-08-08 11:19:48 +0530

    update movie watch list app

--- block-BRaaac/code/script.js
+++ block-BRaaac/code/script.js
@@ -11,7 +11,7 @@ input.addEventListener('keyup', (event) => {
         watched: false,
       });
     }
-    event.target.value = '';;
+    event.target.value = '';
     createUi(allMovies, root);
   }
 });
@@ -21,34 +21,77 @@ function handlechange(event) {
   allMovies[id].watched = !allMovies[id].watched;
   createUi(allMovies, root);
 }
-function elm(type, atr={}){
- let element= document.createElement(type);
+// function elm(type, atr = {}) {
+//   let element = document.createElement(type);
 
-console.log(atr) 
-for(let key in atr){
-    element[key]=atr[key]
-} 
-return element
-}
+//   console.log(atr);
+//   for (let key in atr) {
+//     element[key] = atr[key];
+//   }
+//   return element;
+// }
+
+// function createUi(data, root) {
+//   root.innerHTML = '';
+//   data.forEach((element, i) => {
+//     let li = elm('li');
+//     let span = elm('span' ,{
+//         id : i,
+//         innerText:element.watched ? 'Watched' : 'To Watched'
+//     });
+
+//     let label = elm('label',{
+//         for : i,
+//         innerText:element.name
+//     } );
+
+//     li.append(label, span);
+//     span.addEventListener('click', handlechange);
+//     root.append(li);
+//   });
+// }
+
+// second type 
+
+function elm(type, atr = {}, ...children) {
+    let arr=[];
+    let element = document.createElement(type);
+
+    for (let key in atr) {
+      element[key] = atr[key];
+    }
+ 
+    children.forEach(child=>{
+        if(typeof child==="object"){
+            element.append(child)
+        }
+        if(typeof child==="string"){
+            let node= document.createTextNode(child);
+            element.append(node)
+        }
+    })
+
+     return element;
+  }
 
 
 function createUi(data, root) {
   root.innerHTML = '';
   data.forEach((element, i) => {
-    let li = elm('li');
-    let span = elm('span' ,{
-        id : i,
-        innerText:element.watched ? 'Watched' : 'To Watched'
-    });
-   
-    let label = elm('label',{
-        for : i,
-        innerText:element.name
-    } );
-
-    li.append(label, span);
-    span.addEventListener('click', handlechange);
-    root.append(li);
+    let li = elm(
+      'li',
+      {},
+      elm('label', { for: i }, element.name),
+      elm(
+        'span',
+        {
+          id: i,
+          onclick: handlechange
+        },
+        element.watched ? 'Watched' : 'To Watched'
+      )
+    );
+    root.append(li)
   });
+ 
 }
-
